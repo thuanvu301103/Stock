b38c015f91ff701e8477769f311f1b4200abf8f8
ThuanVu <thuan.vu.e@ctmcorp.com.vn>
2026-01-30 16:39:43 +0700

    feat/clickhose: update util function

--- clickhouse_query/utils/fetch_from_view.sql
+++ clickhouse_query/utils/fetch_from_view.sql
@@ -1,3 +1,7 @@
 SELECT * FROM n8n_olap.v_indicators_base 
 WHERE symbol = 'BTC/USDT'
 ORDER BY trade_date DESC;
+
+SELECT * FROM n8n_olap.v_indicators_normalized 
+WHERE symbol = 'BTC/USDT'
+ORDER BY trade_date DESC;
